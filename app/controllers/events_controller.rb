63b25d88323c9ea73756c90239d9416244a2e1d7
massimo <m.zarola02@gmail.com>
2025-02-20 18:17:14 +0100

    implementazione di base di events_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,24 @@
-class HomeController < ApplicationController
+class EventsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
+    @events = Event.all
   end
+
+  def show
+    @user = current_user
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @product = @user.products.build
+  end
+
+  def edit
+  end
+
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :events
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +10,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(uid: auth.uid).first_or_create do |user|
+      user.provider = auth.info.provider
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.first_name
