78948938bb95189ad2bccb34d1e980686739af82
massimo <m.zarola02@gmail.com>
2025-02-22 13:42:40 +0100

    risolto problema del cambio password per utenti regstrati con google e

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [ :google_oauth2 ]
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
-    where(uid: auth.uid).first_or_create do |user|
-      user.provider = auth.info.provider
+    where(uid: auth.uid, provider: auth.info.provider).first_or_create do |user|
+      user.provider = auth.provider
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.first_name
