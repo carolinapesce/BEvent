da39d4d6cae082f55d7faaba82207dca58c8a97d
massimo <m.zarola02@gmail.com>
2025-03-09 20:33:33 +0100

    fixato piccolo bug del login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
-    where(uid: auth.uid, provider: auth.info.provider).first_or_create do |user|
+    where(uid: auth.uid, provider: auth.provider).first_or_create do |user|
       user.provider = auth.provider
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
