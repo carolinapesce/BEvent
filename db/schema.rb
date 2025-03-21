a512885edb537238882862dd6c8ace5bec7966c8
Kaviu15 <kavidupaula@gmail.com>
2025-03-22 00:56:07 +0100

    aggiunto stile profilo

--- db/schema.rb
+++ db/schema.rb
@@ -91,8 +91,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_21_212341) do
     t.string "city"
     t.string "country"
     t.integer "status", default: 0
-    t.float "latitude"
-    t.float "longitude"
     t.float "price", default: 0.0
     t.integer "user_id", null: false
     t.string "type"
