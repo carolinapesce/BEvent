1beee80438bbca89f45be1073a8e33e2b6b1a582
massimo <m.zarola02@gmail.com>
2025-03-22 11:55:39 +0100

    eliminato price da events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_22_104303) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_22_105257) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -91,7 +91,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_22_104303) do
     t.string "city"
     t.string "country"
     t.integer "status", default: 0
-    t.integer "price"
     t.integer "user_id", null: false
     t.string "type"
     t.boolean "charity_event", default: false
