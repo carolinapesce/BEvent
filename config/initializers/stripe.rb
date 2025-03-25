220ee81ed0660a567eb33d31af4a6e26246cf36d
massimo <m.zarola02@gmail.com>
2025-03-25 22:57:33 +0100

    aggiornato versione API stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
   config.stripe.secret_key = ENV["STRIPE_SECRET_KEY"]
   config.stripe.publishable_key = ENV["STRIPE_PUBLISHABLE_KEY"]
-  Stripe.api_version = '2020-08-27'
+  Stripe.api_version = '2025-02-24.acacia'
 end
