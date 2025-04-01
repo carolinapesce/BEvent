73068955776514be8808ed68f51e3030b169f629
Kaviu15 <kavidupaula@gmail.com>
2025-04-02 01:18:47 +0200

    Fatto primo stile per la parte di Admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,15 @@ module ApplicationHelper
     "#{controller_name} #{action_name}"
   end
 
+  def role_class(role)
+    case role
+    when 0 then 'user'
+    when 1 then 'event-planner'
+    when 2 then 'admin'
+    else ''
+    end
+  end
+
   include Pagy::Frontend
   
 end
