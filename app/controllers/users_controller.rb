ae66453e84857acdeedf50a382941ea4435e4faa
massimo <m.zarola02@gmail.com>
2025-04-03 19:07:59 +0200

    riaggiunto funzione my_events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,25 @@ class UsersController < ApplicationController
     @terminated_events = @user.events.where(status: 'terminated').order(:start_datetime)
   end
 
+  def my_events
+    @events = Event.where(user_id: current_user.id)
+    @query = params[:query]
+    @city = params[:city]
+    @category = params[:category]
+    if @query.present?
+      @events = @events.where("title LIKE ? OR description LIKE ?", "%#{@query}%", "%#{@query}%")
+    end
+
+    if @city.present?
+      @events = @events.where("city LIKE ?", "%#{@city}%")
+    end
+
+    if @category.present?
+      @events = @events.where("category LIKE ?", "%#{@category}%")
+    end
+    render 'users/my_events'
+  end
+
   def show
     @user = User.find(params[:id])
     @upcoming_events = @user.events.where(status: 'upcoming').order(:start_datetime)
